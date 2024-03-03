3836fcfdc88046e3b4be18fab8061efa6acf5249
André Luiz Albano <andre_barros@id.uff.br>
2024-03-03 12:02:40 -0300

    Added classes

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ from numpy.linalg import inv
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import customtkinter as ctk
 from tkinter import filedialog
-from pressure_plot import plot_pressure
+from tkinter import ttk
 
-def plot_pressao(x, y, title, xlabel, ylabel, pressao_df, prof_min, prof_max):
+def plot_pressao(x, y, title, xlabel, ylabel, pressao_df, prof_min, prof_max, mesa_rot):
+    """
+    Função que plota a pressão de formação em função da profundidade (cota)
+    """
     ymin = int(prof_min.get()) if prof_min.get() else min(pressao_df['Prof./Intv.(m)'])
     ymax = int(prof_max.get()) if prof_max.get() else max(pressao_df['Prof./Intv.(m)'])
+
+    ymin = int(mesa_rot.get()) - ymin
+    ymax = int(mesa_rot.get()) - ymax
 
     plt.plot(x, y, 'o')
     plt.title(title)
@@ -24,129 +30,166 @@ def plot_pressao(x, y, title, xlabel, ylabel, pressao_df, prof_min, prof_max):
     plt.grid()
     plt.show()
 
-# importing files
-def plot_pressure():
-    filename = selected_file.get()
-    pressao_df = pd.read_csv(filename, delimiter=';')
-    pressao_df = pressao_df.dropna(subset=['Prof./Intv.(m)', 'Pressão de Formação (Kgf/cm2)'])
+class FileUploader:
+    def __init__(self, master):
+        self.master = master
+        self.fname = tk.StringVar(self.master)
+        self.fname.set("Selecione um arquivo")
+        self.selected_file = tk.StringVar(self.master)
 
-    plot_pressao(x=pressao_df['Pressão de Formação (Kgf/cm2)'],
-                 y=pressao_df['Prof./Intv.(m)'],
-                 title=nome_entry.get(),
-                 xlabel='Pressão de Formação (Kgf/cm2)',
-                 ylabel='Profundidade (m)',
-                 pressao_df=pressao_df,
-                 prof_min=prof_min,
-                 prof_max=prof_max)
+        # Botão para carregar arquivo
+        self.upload_button = ctk.CTkButton(self.master,
+                                           text="Carregar arquivo",
+                                           command=self.upload_file,)
+        self.upload_button.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 
+        # Label to display messages
+        self.message_label = tk.Label(self.master, text="", bg='#ebebeb')
+        self.message_label.grid(row=2, column=0, columnspan=2)
 
-# Upload handeling
-def upload_file():
-    filename = filedialog.askopenfilename()
-    print('Selected:', filename)
+        # Dropdown menu to select file
+        files = os.listdir("./uploads")
+        if not files:
+            files = ["No files found"]
 
-    # Define the destination directory
-    dest_dir = "./uploads"
+        self.dropdown = tk.OptionMenu(self.master, self.fname, *files)
+        self.dropdown.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
 
-    # Create the destination directory if it doesn't exist
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
+        self.fname.trace_add('write', self.update_selected_file)
 
-    # Move the file
-    shutil.copy(filename, dest_dir)
+    def upload_file(self):
+        filename = filedialog.askopenfilename()
+        print(filename)
+        if not os.path.exists("./uploads"):
+            os.makedirs("./uploads")
+        shutil.copy(filename, "./uploads")
+        self.message_label.config(text="File uploaded successfully")
 
-    # Update the text of the message label
-    message_label.config(text="Arquivo carregado com sucesso!")
+    def update_selected_file(self, *args):
+        self.selected_file.set(os.path.join("./uploads", self.fname.get()))
+class WellInfoInput:
+    def __init__(self, master):
+        self.master = master
 
+class ErrorMessage:
+    def __init__(self, master):
+        self.master = master
 
-# Function to reset the plot
-def reset_plot():
-    global canvas
-    if canvas is not None:
-        canvas.get_tk_widget().destroy()
-        canvas = None
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
 
-# Função que automatiza a customização dos botões
-def create_custom_button(root, text, command):
-    return ctk.CTkButton(root,
+        self.geometry("800x600")
+        self.minsize(575, 600)
+        self.title("GradPress")
+        self.iconbitmap(default="./icon.ico")  # icone
+        self.option_add("*Label.font", "Helvetica 15")  # for the font
+
+        # Determinação de um frame para centralizar o conteúdo da página
+        self.frame = tk.Frame(self, bg='#ebebeb')
+        ctk.set_appearance_mode("light")
+        self.frame.place(relx=0.5, rely=0.5, anchor='center')
+
+        # Criação dos frames
+        texto_inicial = "Importe e digite as informações do poço nos campos abaixo."
+        self.label = tk.Label(self.frame, text=texto_inicial, bg='#ebebeb')
+        self.label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+
+        self.file_uploader = FileUploader(self.frame)
+        # # Butão para carregar arquivo
+        # self.upload_button = self.create_custom_button(self.frame, "Carregar arquivo", self.upload_file)
+        # self.upload_button.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+        # self.message_label = tk.Label(self, text="", bg='#ebebeb')
+        # self.message_label.grid(row=2, column=0, columnspan=2)
+
+        # # Dropdown menu para selecionar um arquivo
+        # files = os.listdir("./uploads")
+
+        # # Check if files is empty
+        # if not files:
+        #     files = ["No files found"]
+
+        # self.fname = tk.StringVar(self.frame)
+        # self.fname.set("Select a file")
+        # self.fname.trace_add('write', self.update_selected_file)  # Add this line
+
+        # self.dropdown = tk.OptionMenu(self.frame, self.fname, *files)
+        # self.dropdown.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+        # self.selected_file = tk.StringVar(self.frame)
+
+        # Opções do que se fazer com o arquivo
+        tk.Label(self.frame, text="Digite o nome do poço: ", bg='#ebebeb') \
+            .grid(row=4, columnspan=2, padx=10, pady=10)
+        self.nome_entry = tk.Entry(self.frame, borderwidth=2, width=32)
+        self.nome_entry.grid(row=5, columnspan=2, padx=10, pady=10)
+
+        # Determinação das profundidades
+        tk.Label(self.frame, text="Prof. Inicial", bg='#ebebeb') \
+            .grid(row=6, column=0, padx=10, pady=10)
+        self.prof_min = tk.Entry(self.frame, borderwidth=2, width=16)
+        self.prof_min.grid(row=7, column=0, padx=10, pady=10)
+
+        tk.Label(self.frame, text="Prof. Final", bg='#ebebeb') \
+            .grid(row=8, column=0, padx=10, pady=10)
+        self.prof_max = tk.Entry(self.frame, borderwidth=2, width=16)
+        self.prof_max.grid(row=9, column=0, padx=10, pady=10)
+
+        # Outras opções
+        tk.Label(self.frame, text="Mesa Rotativa*", bg='#ebebeb') \
+            .grid(row=6, column=1, padx=10, pady=10)
+        self.mesa_rot = tk.Entry(self.frame, borderwidth=2, width=16)
+        self.mesa_rot.grid(row=7, column=1, padx=10, pady=10)
+
+        # Cria um StringVar para a mensagem de erro
+        self.error_message = tk.StringVar(self.frame)
+
+        # Cria um Label para exibir a mensagem de erro
+        self.error_label = tk.Label(self.frame, textvariable=self.error_message, fg="red")
+        self.error_label.grid(row=11, column=0, columnspan=2, padx=10, pady=10)
+
+        # Butão de plotagem
+        self.create_custom_button(root=self.frame, text="Plotar", command=self.plot_pressure) \
+            .grid(row=10, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
+
+    def create_custom_button(self, root, text, command):
+        # Your implementation of create_custom_button goes here
+        return ctk.CTkButton(root,
                          text=text,
                          command=command,
                          corner_radius=10,
                          hover_color="#104a78",
                          width=200,
-                         font=("Arial", 15, "bold"))
+                         font=("Helvetica", 15, "bold"))
 
-def update_selected_file(*args):
-    selected_file.set(os.path.join("./uploads", fname.get()))
+    def plot_pressure(self):
+        """
+        Plota os dados de pressão em relação à profundidade.
 
-# Criando a janela principal, o título e o ícone
-root = tk.Tk()
-root.title("GradPress")
-root.iconbitmap(default="./icon.ico") #icone
-root.option_add("*Label.Font", "Poppins 15") #for the font
-root.configure(bg='#F2F2F2') # Trocando o pano de fundo
+        Esta função lê um arquivo CSV contendo dados de pressão e os plota em relação
+        aos valores de profundidade (em cota) correspondentes.
+        Elimina quaisquer linhas com valores NaN.
+        Os valores de profundidade em cota são calculados subtraindo
+        os valores de 'Prof./Intv.(m)' do valor de 'mesa_rot'.
+        """
+        try:
+            filename = self.file_uploader.selected_file.get()
+            pressao_df = pd.read_csv(filename, delimiter=';')
+            pressao_df = pressao_df.dropna(subset=['Prof./Intv.(m)', 'Pressão de Formação (Kgf/cm2)'])
+            prof_cota = int(self.mesa_rot.get()) - pressao_df['Prof./Intv.(m)']
 
+            plot_pressao(x=pressao_df['Pressão de Formação (Kgf/cm2)'],
+                        y=prof_cota,
+                        title=self.nome_entry.get(),
+                        xlabel='Pressão de Formação (Kgf/cm2)',
+                        ylabel='Profundidade (m)',
+                        pressao_df=pressao_df,
+                        prof_min=self.prof_min,
+                        prof_max=self.prof_max,
+                        mesa_rot=self.mesa_rot)
+        except Exception as e:
+            self.error_message.set(str(e))
 
-# Criação dos frames
-texto_inicial = "Importe e digite as informações do poço nos campos abaixo."
-label = tk.Label(root, text=texto_inicial,bg='#F2F2F2')
-label.grid(row=0,column=0, columnspan=2,padx=10, pady=10)
-
-# Butão para carregar arquivo
-
-upload_button = create_custom_button(root, "Carregar arquivo", upload_file)
-upload_button.grid(row=1,column=0, columnspan=2,padx=10, pady=10)
-message_label = tk.Label(root, text="",bg='#F2F2F2')
-message_label.grid(row=2,column=0, columnspan=2)
-
-# INPUT
-# tk.Label(root, text="Digite o passo: ").pack(padx=10, pady=10)
-# passo_entry = tk.Entry(root, borderwidth=2)
-# passo_entry.pack(padx=10, pady=10)
-
-# Dropdown menu to select file
-files = os.listdir("./uploads")
-
-# Check if files is empty
-if not files:
-    files = ["No files found"]
-
-fname = tk.StringVar(root)
-fname.set("Select a file")
-fname.trace_add('write', update_selected_file)  # Add this line
-
-dropdown = tk.OptionMenu(root, fname, *files)
-dropdown.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
-
-selected_file = tk.StringVar(root)
-
-# Opções do que se fazer com o arquivo
-tk.Label(root, text="Digite o nome do poço: ",bg='#F2F2F2')\
-    .grid(row=4, columnspan=2,padx=10, pady=10)
-nome_entry = tk.Entry(root, borderwidth=2, width=32)
-nome_entry.grid(row=5, columnspan=2,padx=10, pady=10)
-
-# Determinação das profundidades
-tk.Label(root, text="Prof. A",bg='#F2F2F2')\
-    .grid(row=6,column=0,padx=10, pady=10)
-prof_min = tk.Entry(root, borderwidth=2, width=16)
-prof_min.grid(row=7,column=0,padx=10, pady=10)
-
-tk.Label(root, text="Prof. B",bg='#F2F2F2')\
-    .grid(row=8,column=0,padx=10, pady=10)
-prof_max = tk.Entry(root, borderwidth=2, width=16)
-prof_max.grid(row=9,column=0,padx=10, pady=10)
-
-# Outras opções
-tk.Label(root, text="Mesa Rotativa", bg='#F2F2F2')\
-    .grid(row=6,column=1,padx=10, pady=10)
-mesa_rot = tk.Entry(root, borderwidth=2, width=16)\
-    .grid(row=7,column=1,padx=10, pady=10)
-
-create_custom_button(root, "Submit", plot_pressure)\
-    .grid(row=9,column=1,padx=10, pady=10)
-
-
-
-# Start the event loop
-root.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
