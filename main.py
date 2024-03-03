2f33bec528d0852d71a0ef19cb7fb4202eaaf1a9
André Luiz Albano <andre_barros@id.uff.br>
2024-03-03 16:14:46 -0300

    v0.0.2
    Added footer

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ import numpy as np
 import pandas as pd
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from pandasgui import show
-from tkinter import filedialog, ttk
+from tkinter import filedialog, ttk, Label, PhotoImage
 import tkinter as tk
 from utilities import create_custom_button
+from PIL import Image, ImageTk
+
 class FileUploader:
     def __init__(self, master):
         self.master = master
-
         ######################################################
         self.upload_frame = tk.Frame(self.master, bg='#ebebeb')
         self.upload_frame.grid(row=0, column=0, columnspan=2,padx=10, pady=10)
         ######################################################
+        # giecar logo handling
+        self.img = Image.open("./img/giecar.png")
+        or_width, or_height = self.img.size
+        self.img = self.img.resize((or_width//3, or_height//3))
+        self.photo_img = ImageTk.PhotoImage(self.img)
+        img_giecar = Label(self.upload_frame, image=self.photo_img, bg='#ebebeb')
+        img_giecar.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
         self.fname = tk.StringVar(self.master)
         self.fname.set("Selecione um arquivo")
@@ -25,7 +33,7 @@ class FileUploader:
         # Texto inicial
         texto_inicial = "Importe e digite as informações do poço nos campos abaixo."
         self.label = tk.Label(self.upload_frame, text=texto_inicial, bg='#ebebeb')
-        self.label.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+        self.label.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
 
 
         # Botão para carregar arquivo
@@ -33,11 +41,11 @@ class FileUploader:
                                            text="Carregar arquivo",
                                            command=self.upload_file,)
         # then the grid within the frame
-        self.upload_button.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+        self.upload_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
         # Label to display messages
         self.message_label = tk.Label(self.upload_frame, text="", bg='#ebebeb')
-        self.message_label.grid(row=2, column=0, columnspan=2)
+        self.message_label.grid(row=3, column=0, columnspan=2)
 
         # Dropdown menu to select file
         files = os.listdir("./uploads")
@@ -45,12 +53,12 @@ class FileUploader:
             files = ["Nenhum arquivo encontrado"]
 
         self.dropdown = tk.OptionMenu(self.upload_frame, self.fname, *files)
-        self.dropdown.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+        self.dropdown.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
 
         self.view_button = create_custom_button(self.upload_frame,
                                                 "Visualizar arquivo",
                                                 self.view_file)
-        self.view_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
+        self.view_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
 
         self.fname.trace_add('write', self.update_selected_file)
 
@@ -85,24 +93,40 @@ class WellInfoInput:
         # Input do nome do poço
         tk.Label(self.well_info_frame, text="Digite o nome do poço: ", bg='#ebebeb') \
             .grid(row=0, columnspan=2, padx=10, pady=10)
-        self.nome_entry = tk.Entry(self.well_info_frame, borderwidth=2, width=32)
+        self.nome_entry = tk.Entry(self.well_info_frame,
+                                   borderwidth=1,
+                                   width=45,
+                                   justify="center",
+                                   relief="solid")
         self.nome_entry.grid(row=1, columnspan=2, padx=10, pady=10)
 
         # Depth inputs
         tk.Label(self.well_info_frame, text="Prof. Inicial", bg='#ebebeb') \
             .grid(row=2, column=0, padx=10, pady=10)
-        self.prof_min = tk.Entry(self.well_info_frame, borderwidth=2, width=16)
+        self.prof_min = tk.Entry(self.well_info_frame,
+                                 borderwidth=1,
+                                 width=18,
+                                 justify="center",
+                                 relief="solid")
         self.prof_min.grid(row=3, column=0, padx=10, pady=10)
 
         tk.Label(self.well_info_frame, text="Prof. Final", bg='#ebebeb') \
             .grid(row=4, column=0, padx=10, pady=10)
-        self.prof_max = tk.Entry(self.well_info_frame, borderwidth=2, width=16)
+        self.prof_max = tk.Entry(self.well_info_frame,
+                                 borderwidth=1,
+                                 width=18,
+                                 justify="center",
+                                 relief="solid")
         self.prof_max.grid(row=5, column=0, padx=10, pady=10)
 
         # Other options
         tk.Label(self.well_info_frame, text="Mesa Rotativa*", bg='#ebebeb') \
             .grid(row=2, column=1, padx=10, pady=10)
-        self.mesa_rot = tk.Entry(self.well_info_frame, borderwidth=2, width=16)
+        self.mesa_rot = tk.Entry(self.well_info_frame,
+                                 borderwidth=1,
+                                 width=18,
+                                 justify="center",
+                                 relief="solid")
         self.mesa_rot.grid(row=3, column=1, padx=10, pady=10)
 
         # Error message
@@ -165,12 +189,41 @@ class WellInfoInput:
         except Exception as e:
             self.error_message.set(str(e))
 
+class Footer:
+    def __init__(self, master):
+        import webbrowser
+        self.master = master
+        self.footer_frame = tk.Frame(self.master, bg='#ebebeb')
+        # Se eu quiser adicionar mais alguma coisa antes do footer
+        # alterar o argumento row abaixo.
+        self.footer_frame.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+        giecar_link = create_custom_button(self.footer_frame,
+                                           text="GIECAR",
+                                           command=lambda: \
+                                           webbrowser.open("http://gcr.sites.uff.br/"),
+                                           width=100)
+        giecar_link.grid(row=0, column=0, padx=10, pady=10)
+
+        github_link = create_custom_button(self.footer_frame,
+                                           text="GitHub",
+                                           command=lambda: \
+                                           webbrowser.open("https://github.com/albano-a/GradPress"),
+                                           width=100)
+        github_link.grid(row=0, column=1, padx=10, pady=10)
+
+        exit_button = create_custom_button(self.footer_frame,
+                                           text="Sair",
+                                           command=self.master.quit,
+                                           width=100)
+        exit_button.grid(row=0, column=2, padx=10, pady=10)
+
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         ctk.set_appearance_mode("light")
-        self.geometry("800x600")
-        self.minsize(575, 600)
+        self.geometry("600x720")
+        self.minsize(600, 830)
         self.title("GradPress")
         self.iconbitmap(default="./icon.ico")  # icone
         self.option_add("*Label.font", "Helvetica 15")  # for the font
@@ -182,7 +235,9 @@ class App(ctk.CTk):
 
         self.file_uploader = FileUploader(self.main_frame)
         self.well_info_input = WellInfoInput(self.main_frame, self.file_uploader)
-        # self.error_message = ErrorMessage(self.frame)
+        self.footer = Footer(self.main_frame)
+
+
 
 
 if __name__ == "__main__":
--- utilities.py
+++ utilities.py
@@ -1,11 +1,13 @@
 import customtkinter as ctk
 
-def create_custom_button(root, text, command):
+def create_custom_button(root, text, command, width=200):
         # Your implementation of create_custom_button goes here
         return ctk.CTkButton(root,
                             text=text,
                             command=command,
                             corner_radius=10,
+                            border_width=1,
+                            border_color="#aeaeae",
                             hover_color="#104a78",
-                            width=200,
+                            width=width,
                             font=("Helvetica", 15, "bold"))
