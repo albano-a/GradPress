c5da744ce98b7789d58e5f1fb96b30932f90320d
André Luiz Albano <andre_barros@id.uff.br>
2024-03-03 18:04:07 -0300

    v0.0.4
    A lot of changes
    Improved UI

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from pandasgui import show
-from tkinter import filedialog, ttk, Label, PhotoImage
+import pandasgui
+from tkinter import filedialog, ttk, Label
 import tkinter as tk
-from utilities import create_custom_button
+from utilities import create_custom_button, custom_dropdown
 from PIL import Image, ImageTk
 
 class FileUploader:
@@ -26,15 +26,10 @@ class FileUploader:
         img_giecar = Label(self.upload_frame, image=self.photo_img, bg='#ebebeb')
         img_giecar.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
-        self.fname = tk.StringVar(self.master)
-        self.fname.set("Selecione um arquivo")
-        self.selected_file = tk.StringVar(self.master)
-
         # Texto inicial
         texto_inicial = "Importe e digite as informações do poço nos campos abaixo."
         self.label = tk.Label(self.upload_frame, text=texto_inicial, bg='#ebebeb')
         self.label.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
-
 
         # Botão para carregar arquivo
         self.upload_button = create_custom_button(self.upload_frame,
@@ -43,22 +38,25 @@ class FileUploader:
         # then the grid within the frame
         self.upload_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
-        # Label to display messages
-        self.message_label = tk.Label(self.upload_frame, text="", bg='#ebebeb')
-        self.message_label.grid(row=3, column=0, columnspan=2)
 
         # Dropdown menu to select file
+        self.fname = tk.StringVar(self.upload_frame)
+        self.fname.set("Selecione um arquivo")
+        self.selected_file = tk.StringVar(self.master)
+
         files = os.listdir("./uploads")
         if not files:
             files = ["Nenhum arquivo encontrado"]
 
-        self.dropdown = tk.OptionMenu(self.upload_frame, self.fname, *files)
-        self.dropdown.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
+        self.dropdown = custom_dropdown(self.upload_frame,
+                                        values=files,
+                                        variable=self.fname)
+        self.dropdown.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
 
         self.view_button = create_custom_button(self.upload_frame,
                                                 "Visualizar arquivo",
                                                 self.view_file)
-        self.view_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+        self.view_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
 
         self.fname.trace_add('write', self.update_selected_file)
 
@@ -66,9 +64,9 @@ class FileUploader:
         filename = self.selected_file.get()
         if os.path.exists(filename):
             df = pd.read_csv(filename, sep=";")
-            show(df)
+            pandasgui.show(df)
         else:
-            self.messagebox.showerror("Error", "Arquivo não encontrado!")
+            tk.messagebox.showerror("Error", "Arquivo não encontrado!")
 
     def upload_file(self):
         filename = filedialog.askopenfilename()
@@ -76,7 +74,8 @@ class FileUploader:
         if not os.path.exists("./uploads"):
             os.makedirs("./uploads")
         shutil.copy(filename, "./uploads")
-        self.message_label.config(text="Arquivo carregado com sucesso!")
+        # self.message_label.config(text="Arquivo carregado com sucesso!")
+        tk.messagebox.showinfo("Sucesso", "Arquivo carregado com sucesso!")
 
     def update_selected_file(self, *args):
         self.selected_file.set(os.path.join("./uploads", self.fname.get()))
@@ -90,56 +89,65 @@ class WellInfoInput:
         self.well_info_frame.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
         ######################################################
 
+        texto_inicial = "Preencha as informações abaixo para plotar o gráfico de pressão.\n *Obrigatório"
+        self.label = tk.Label(self.well_info_frame, text=texto_inicial, bg='#ebebeb')
+        self.label.grid(row=0, column=0, columnspan=2, padx=15, pady=10)
+
         # Input do nome do poço
         tk.Label(self.well_info_frame, text="Digite o nome do poço: ", bg='#ebebeb') \
-            .grid(row=0, columnspan=2, padx=10, pady=10)
+            .grid(row=1, columnspan=2, padx=10, pady=10)
         self.nome_entry = tk.Entry(self.well_info_frame,
                                    borderwidth=1,
-                                   width=45,
+                                   width=42,
                                    justify="center",
-                                   relief="solid")
-        self.nome_entry.grid(row=1, columnspan=2, padx=10, pady=10)
+                                   relief="solid",
+                                   font=("Helvetica", 12))
+        self.nome_entry.grid(row=2, columnspan=2, padx=10, pady=10)
 
         # Depth inputs
         tk.Label(self.well_info_frame, text="Prof. Inicial", bg='#ebebeb') \
-            .grid(row=2, column=0, padx=10, pady=10)
+            .grid(row=3, column=0, padx=10, pady=10)
         self.prof_min = tk.Entry(self.well_info_frame,
                                  borderwidth=1,
                                  width=18,
                                  justify="center",
-                                 relief="solid")
-        self.prof_min.grid(row=3, column=0, padx=10, pady=10)
+                                 relief="solid",
+                                 font=("Helvetica", 12))
+        self.prof_min.grid(row=4, column=0, padx=10, pady=10)
 
         tk.Label(self.well_info_frame, text="Prof. Final", bg='#ebebeb') \
-            .grid(row=4, column=0, padx=10, pady=10)
+            .grid(row=5, column=0, padx=10, pady=10)
         self.prof_max = tk.Entry(self.well_info_frame,
                                  borderwidth=1,
                                  width=18,
                                  justify="center",
-                                 relief="solid")
-        self.prof_max.grid(row=5, column=0, padx=10, pady=10)
+                                 relief="solid",
+                                 font=("Helvetica", 12))
+        self.prof_max.grid(row=6, column=0, padx=10, pady=10)
 
         # Other options
         tk.Label(self.well_info_frame, text="Mesa Rotativa*", bg='#ebebeb') \
-            .grid(row=2, column=1, padx=10, pady=10)
+            .grid(row=3, column=1, padx=10, pady=10)
         self.mesa_rot = tk.Entry(self.well_info_frame,
                                  borderwidth=1,
                                  width=18,
                                  justify="center",
-                                 relief="solid")
-        self.mesa_rot.grid(row=3, column=1, padx=10, pady=10)
+                                 relief="solid",
+                                 font=("Helvetica", 12))
+        self.mesa_rot.grid(row=4, column=1, padx=10, pady=10)
 
         # Error message
         self.error_message = tk.StringVar(self.well_info_frame)
         self.error_label = tk.Label(self.well_info_frame,
                                     textvariable=self.error_message,
                                     fg="red")
-        self.error_label.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+        self.error_label.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
         create_custom_button(root=self.well_info_frame,
                             text="Plotar",
-                            command=self.plot_pressure)\
-            .grid(row=6, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
+                            command=self.plot_pressure,
+                            width=300)\
+            .grid(row=7, column=0, columnspan=2, padx=10, pady=10)
 
     def plot_pressao(self, x, y, title, xlabel, ylabel, pressao_df, prof_min, prof_max, mesa_rot):
         """
@@ -189,6 +197,20 @@ class WellInfoInput:
         except Exception as e:
             self.error_message.set(str(e))
 
+class Calculations:
+    def __init__(self, master):
+        self.master = master
+        self.calc_frame = tk.Frame(self.master, bg='#ebebeb')
+        self.calc_frame.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+        self.calc_button = create_custom_button(self.calc_frame,
+                                                text="Calcular",
+                                                command=self.calculate)
+        self.calc_button.grid(row=0, column=0, padx=10, pady=10)
+
+    def calculate(self):
+        pass
+
 class Footer:
     def __init__(self, master):
         import webbrowser
@@ -218,11 +240,17 @@ class Footer:
                                            width=100)
         exit_button.grid(row=0, column=2, padx=10, pady=10)
 
+        texto_inicial = "Desenvolvido por André Albano\nGIECAR - UFF\n2024"
+        self.label = tk.Label(self.footer_frame, text=texto_inicial, bg='#ebebeb', font=("Helvetica", 10))
+        self.label.grid(row=1, column=0, columnspan=3, padx=15, pady=10)
+
+
+
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         ctk.set_appearance_mode("light")
-        self.geometry("600x720")
+        self.geometry("600x900")
         self.minsize(600, 830)
         self.title("GradPress")
         self.iconbitmap(default="./icon.ico")  # icone
--- utilities.py
+++ utilities.py
@@ -4,10 +4,28 @@ def create_custom_button(root, text, command, width=200):
         # Your implementation of create_custom_button goes here
         return ctk.CTkButton(root,
                             text=text,
+                            fg_color="#23479e",
                             command=command,
                             corner_radius=10,
                             border_width=1,
                             border_color="#aeaeae",
-                            hover_color="#104a78",
+                            hover_color="#0a0f53",
                             width=width,
                             font=("Helvetica", 15, "bold"))
+
+
+def custom_dropdown(root, values, variable, width=200):
+        # Your implementation of custom_dropdown goes here
+        return ctk.CTkComboBox(root,
+                               values=values,
+                               variable=variable,
+                               corner_radius=10,
+                               fg_color="#fff",
+                               button_color="#23479e",
+                               width=width,
+                               button_hover_color="#0a0f53",
+                               text_color="#212121",
+                               border_color="#aeaeae",
+                               border_width=1,
+                               hover=True,
+                               font=("Helvetica", 15, "bold"))
