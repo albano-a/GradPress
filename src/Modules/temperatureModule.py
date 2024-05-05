6781a96d6105a93a33a669bfae55d4a2971148cc
[Andre Albano] <110417140+albano-a@users.noreply.github.com>
2024-05-05 01:16:51 -0300

    chore: Add configuration plot dialog and toolbar functionality

--- src/Modules/temperatureModule.py
+++ src/Modules/temperatureModule.py
@@ -5,11 +5,13 @@ import pandas as pd
 from Functions import temperature as temp
 from Functions.general import timing_function
 from Interface.pyInterface.temperatureInterface import Ui_mainWindow
+from Interface.pyInterface.temperatureInterfaceDialog import Ui_configurationPlotDialog
 from PyQt6.QtWidgets import (
     QMainWindow,
     QMessageBox,
     QVBoxLayout,
     QFileDialog,
+    QDialog,
     QApplication,
 )
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
@@ -17,6 +19,12 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT
 from matplotlib.figure import Figure
 
 plt.style.use(["bmh"])
+
+
+class ConfigurationDialog(QDialog, Ui_configurationPlotDialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 
 class TemperatureAnalysis(QMainWindow, Ui_mainWindow):
@@ -31,6 +39,21 @@ class TemperatureAnalysis(QMainWindow, Ui_mainWindow):
         self.__plotLayout.addWidget(self.canvas)
 
         self.importFileToolBtn.clicked.connect(self.importFile)
+        self.plotButtonTemp.clicked.connect(self.plotTemperature)
+        self.configurationButton.clicked.connect(self.openPlotConfigurationDialog)
+
+        self.matplotlib_toolbar = NavigationToolbar2QT(self.canvas)
+
+        self.homePlotButton.clicked.connect(self.matplotlib_toolbar.home)
+        self.backwardPlotButton.clicked.connect(self.matplotlib_toolbar.back)
+        self.forwardPlotButton.clicked.connect(self.matplotlib_toolbar.forward)
+        self.panPlotButton.clicked.connect(self.matplotlib_toolbar.pan)
+        self.zoomPlotButton.clicked.connect(self.matplotlib_toolbar.zoom)
+        self.editPlotButton.clicked.connect(self.matplotlib_toolbar.edit_parameters)
+        self.configureSubplotsButton.clicked.connect(
+            self.matplotlib_toolbar.configure_subplots
+        )
+        self.savePlotButton.clicked.connect(self.matplotlib_toolbar.save_figure)
 
     def importFile(self):
         fileDialog = QFileDialog()
@@ -44,5 +67,12 @@ class TemperatureAnalysis(QMainWindow, Ui_mainWindow):
         if filePath:
             fileDirName = os.path.dirname(filePath)
             self.inputFilePath.setText(fileDirName)
-    
-    
+
+        return fileDirName
+
+    def openPlotConfigurationDialog(self):
+        dialog = ConfigurationDialog()
+        dialog.exec()
+
+    def plotTemperature(self):
+        pass
